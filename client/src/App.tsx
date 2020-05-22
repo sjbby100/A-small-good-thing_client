42d2db840a1a7bee56563fa7ea1cec8ef30f1380
oouuyab <01073785645@naver.com>
2020-05-22 16:27:59 +0900

    Add route, signin css, validate and submit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,17 @@ import { Route, Switch } from "react-router-dom";
 import { bugAdded } from "./modules/bugs";
 import store from "./store";
 import Signup from "./pages/signup";
+import Signin from "./pages/signin";
+import Home from "./pages/home";
 export default class App extends Component {
   render() {
     store.dispatch(bugAdded("에어팟", 222000, "사고싶다"));
     return (
       <div>
         <Switch>
-          {/* <Route path="/signin" component={Signin} /> */}
+          <Route path="/signin" component={Signin} />
           <Route path="/signup" component={Signup} />
+          <Route path="/home" component={Home} />
         </Switch>
       </div>
     );
--- client/src/pages/signin.tsx
+++ client/src/pages/signin.tsx
@@ -1,26 +1,29 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { connect } from "react-redux";
-//import * as igninActions from "../modules/signin";
-//import { Redirect } from "react-router-dom";
+import { Redirect, Link, RouteComponentProps } from "react-router-dom";
 import * as SigninInput from "../components/signin_Input";
+import "../pages/css/signin.css";
 //* fake data
 import { fake } from "../services/fakeData";
 // todo [] error message 받는 방법 찾기
-
-//state type 지정 : interface 사용
 interface state {
   email: string;
   password: string;
   errors: {};
 }
+interface Props extends RouteComponentProps {}
+//state type 지정 : interface 사용
 
-class Signin extends Component {
-  state: state = {
-    email: "",
-    password: "",
-    errors: {},
-  };
+class Signin extends React.Component<Props, state> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+  }
   handleChange = ({ currentTarget }: any) => {
     //email 변경
     const { value, name } = currentTarget;
@@ -30,36 +33,63 @@ class Signin extends Component {
     });
   };
 
-  handleSignin = async (e: any) => {
+  //check =()=>{}
+
+  handleSignin = (e: any) => {
     e.preventDefault();
-    const { email, password } = this.state;
-    const { errors } = SigninInput.validateInput(email, password);
+    const { email, password, errors } = this.state;
+    const { vali_error } = SigninInput.validateInput(password);
     console.log(this.state);
-    console.log(errors);
-    if (errors.emailErr === "" && errors.pwErr === "") {
-      let post = await axios.post("http://localhost:8080/user/login", {
-        data: {
-          email,
-          password,
-        },
-      });
+
+    if (vali_error === "" && Object.keys(errors).length === 0) {
+      axios
+        .post("http://localhost:4000/signin", {
+          data: {
+            email,
+            password,
+          },
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            // ! state 관리! redux로!!!
+            console.log("$c 로그인 성공!", "color : green");
+            this.props.history.replace("/home");
+            //return <Redirect path="/signin" to="/home" />;
+          } else if (res.status === 403) {
+            console.log("%c이미 존재하는 이메일입니다.", "color:red");
+          } else if (res.status === 401) {
+            console.log("%c비밀번호를 확인해주세요.", "color:red");
+          }
+        });
     }
   };
   render() {
     const { email, password } = this.state;
     const { handleChange, handleSignin } = this;
 
+    //! form 안에다 넣어야 관리가 쉬워짐
     return (
-      <div>
-        {/*form 안에다 넣어야 관리가 쉬워짐 */}
-        <form onSubmit={handleSignin}>
-          <SigninInput.Input
-            email={email}
-            password={password}
-            onChange={handleChange}
-          />
-          <button>로그인</button>
-        </form>
+      <div className="container">
+        <div className="signin_wrapper">
+          <div className="textbox">
+            <h1>
+              안녕하세요. 굿띵은
+              <br />
+              로그인이 필요한 서비스 입니다.
+            </h1>
+          </div>
+          <form onSubmit={handleSignin} className="form" autoComplete="off">
+            <SigninInput.Input
+              email={email}
+              password={password}
+              onChange={handleChange}
+            />
+            <button className="submit">로그인</button>
+          </form>
+          <Link to={"/signup"} className="signin_back">
+            아이디가 없으신가요?
+          </Link>
+        </div>
       </div>
     );
   }
